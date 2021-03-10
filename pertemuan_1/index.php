0634702d2ae7e3f3847ed52c8d5c0e35a5eba6b5
joo <arihasyim.zii@gmail.com>
2021-03-10 19:53:24 +0700

    pertemuan1

--- pertemuan_1/index.php
+++ pertemuan_1/index.php
@@ -1,3 +1,17 @@
+<?php
+    // tipe data
+    $int = 12;
+    $str = "asd";
+    $dbl = 1.1;
+    $arr = ["a", "b", "c"];
+
+    // array associative
+    $arr2 = [
+        'nama' => 'asd',
+        'nim' => '01',
+        'umur' => '20'
+    ];
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,7 +20,51 @@
 </head>
 <body>
     <?php
-        echo "Hello World!";
+        // cara print
+        echo $int;
+        echo $str;
     ?>
+    <!-- ///////////// -->
+
+    <?php
+    // looping
+        for($i = 0; $i < 5; $i++) {
+
+            // if else
+            if($i % 2 == 0) { ?>
+                <p>genap</p>
+            <?php } else { ?>
+                <p>ganjil</p>
+            <?php }
+
+        } ?>
+    <!-- ///////////// -->
+
+    <?php 
+        // penjumlahan string
+        echo $str . " " . $str;
+    ?>
+    <!-- ///////////// -->
+
+    <?php 
+        // vardump
+        var_dump($arr);
+    ?>
+    <!-- ///////////// -->
+
+    <?php 
+        //foreach array
+        foreach($arr as $a) {
+            echo $a . " ";
+        }
+    ?>
+    <!-- ///////////// -->
+    
+    <?php 
+        //echo array
+        echo $arr[1];
+        echo $arr2["nim"]
+    ?>
+    
 </body>
 </html>
